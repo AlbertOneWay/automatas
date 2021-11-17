43fb96a2334a7b79adc6bd7ed957be321f71bf69
Albert Muñoz <albert.1701920351@ucaldas.edu.co>
2021-11-17 16:54:02 -0500

    Incorporacion de metodos aleatorios y sonidos

--- source/index.js
+++ source/index.js
@@ -2,19 +2,93 @@ import { instrumentos } from "./model/instrumentos.js"
 
 document.addEventListener("DOMContentLoaded", e => {
 
-    const automata1 = []
-    const automata2 = []
-    const automata3 = []
-
-
 
     const instrumento1 = EscogerInstrumento("guitarra")
     const instrumento2 = EscogerInstrumento("violin")
     const instrumento3 = EscogerInstrumento("acordeon")
 
-    console.log(InstrumentoRandom());
+    let instru = InstrumentoRandom()
+    
+    console.log(NotaRandom("guitarra")); 
 
+    let config = LlenarConfigInicial("010111", 6);
+    console.log(config);
+
+    config = ConfigInicialRandom(LongitudRandom());
+    console.log(config);
 })
+
+function CrearAutomata({notacero, notauno, instrumento, configuracionInicial}){
+
+    if(!instrumento){
+        instrumento= InstrumentoRandom()
+    }
+    if(!notacero){
+        notacero= NotaRandom(instrumento)
+    }
+    if(!notauno){
+        notauno= NotaRandom(instrumento)
+    }
+    if(!configuracionInicial.longitud){
+        configuracionInicial.longitud = LongitudRandom()
+    }
+
+    configuracionInicial.longitud  
+
+    const automata = {
+        notacero: notacero,
+        notauno: notauno,
+        instrumento: instrumento,
+        array: []
+    }
+
+}
+
+function LlenarConfigInicial (cadena, longitud){
+
+    if(cadena.length === longitud){
+
+        const array = []
+
+        cadena.split("").forEach(element => {
+            array.push(parseInt(element))
+        })
+
+        return array
+    } else {
+        console.log("La cadena no es igual a la longitud determinada");
+    }
+
+}
+
+function ConfigInicialRandom (longitud){
+
+    const array = []
+
+   for(let i=0 ; i<longitud; i++){
+       const estadoAleatorio = Math.round(Math.random()) 
+       array.push(estadoAleatorio)
+   }
+
+   return array
+}
+
+function EscogerLongitud(longitud){
+
+    if(longitud > 100){
+        longitud = 100
+    }
+
+    return longitud
+}
+
+function LongitudRandom(){
+
+    let numero = Math.round(Math.random() * 99) + 1
+
+    return numero
+}
+
 
 function EscogerInstrumento(instrumento) {
 
@@ -31,4 +105,23 @@ function InstrumentoRandom() {
     console.log(numero);
 
     return Object.values(instrumentos)[numero]
+
+}
+
+function EscogerNota(instrumento, nota) {
+    
+    return instrumentos[instrumento][nota]
+
+}
+
+function NotaRandom(instrumento) {
+    
+    let numero = Math.round((Math.random() * 4));
+
+    console.log(numero);
+
+    console.log(Object.values(instrumentos[instrumento]));
+
+    return Object.values(instrumentos[instrumento])[numero]
+
 }
--- source/model/instrumentos.js
+++ source/model/instrumentos.js
@@ -1,63 +1,73 @@
 export const instrumentos = {
     "guitarra": {
-        do: "guitarra",
-        re: new Audio(""),
-        mi: new Audio(""),
-        fa: new Audio("")
+        "silencio": new Audio(""),
+        "do": new Audio("./sounds/Guitarra/G1.mp3"),
+        "re": new Audio("./sounds/Guitarra/G2.mp3"),
+        "mi": new Audio("./sounds/Guitarra/G3.mp3"),
+        "fa": new Audio("./sounds/Guitarra/G4.mp3")
     },
     "trompeta": {
-        do: "guitatrompetarra",
-        re: new Audio(""),
-        mi: new Audio(""),
-        fa: new Audio("")
-    },
-    "clarinete": {
-        do: "clarinete",
-        re: new Audio(""),
-        mi: new Audio(""),
-        fa: new Audio("")
-    },
-    "trombon": {
-        do: "trombon",
-        re: new Audio(""),
-        mi: new Audio(""),
-        fa: new Audio("")
-    },
-    "acordeon": {
-        do: "acordeon",
-        re: new Audio(""),
-        mi: new Audio(""),
-        fa: new Audio("")
-    },
-    "violin": {
-        do: "violin",
-        re: new Audio(""),
-        mi: new Audio(""),
-        fa: new Audio("")
-    },
-    "saxofon": {
-        do: "saxofon",
-        re: new Audio(""),
-        mi: new Audio(""),
-        fa: new Audio("")
+        "silencio": new Audio("./sounds/silencio.mp3"),
+        "do": new Audio("./sounds/Trompeta/T1.mp3"),
+        "re": new Audio("./sounds/Trompeta/T2.mp3"),
+        "mi": new Audio("./sounds/Trompeta/T3.mp3"),
+        "fa": new Audio("./sounds/Trompeta/T4.mp3")
     },
     "bajo": {
-        do: "bajo",
-        re: new Audio(""),
-        mi: new Audio(""),
-        fa: new Audio("")
+        "silencio": new Audio("./sounds/silencio.mp3"),
+        "do": new Audio("./sounds/Bajo/B1.mp3"),
+        "re": new Audio("./sounds/Bajo/B2.mp3"),
+        "mi": new Audio("./sounds/Bajo/B3.mp3"),
+        "fa": new Audio("./sounds/Bajo/B4.mp3")
+    },
+    "trombon": {
+        "silencio": new Audio("./sounds/silencio.mp3"),
+        "do": new Audio("./sounds/Trombon/TR1.mp3"),
+        "re": new Audio("./sounds/Trombon/TR2.mp3"),
+        "mi": new Audio("./sounds/Trombon/TR3.mp3"),
+        "fa": new Audio("./sounds/Trombon/TR4.mp3")
+    },
+    "acordeon": {
+        "silencio": new Audio("./sounds/silencio.mp3"),
+        "do": new Audio("./sounds/Acordeon/A1.mp3"),
+        "re": new Audio("./sounds/Acordeon/A2.mp3"),
+        "mi": new Audio("./sounds/Acordeon/A3.mp3"),
+        "fa": new Audio("./sounds/Acordeon/A4.mp3")
+    },
+    "violin": {
+        "silencio": new Audio("./sounds/silencio.mp3"),
+        "do": new Audio("./sounds/Violin/V1.wav"),
+        "re": new Audio("./sounds/Violin/V2.wav"),
+        "mi": new Audio("./sounds/Violin/V3.wav"),
+        "fa": new Audio("./sounds/Violin/V4.wav")
+    },
+    "ukelele": {
+        "silencio": new Audio("./sounds/silencio.mp3"),
+        "do": new Audio("./sounds/Ukelele/U1.mp3"),
+        "re": new Audio("./sounds/Ukelele/U2.mp3"),
+        "mi": new Audio("./sounds/Ukelele/U3.mp3"),
+        "fa": new Audio("./sounds/Ukelele/U4.mp3")
+    },
+    "flauta": {
+        "silencio": new Audio("./sounds/silencio.mp3"),
+        "do": new Audio("./sounds/Flauta/F1.mp3"),
+        "re": new Audio("./sounds/Flauta/F2.mp3"),
+        "mi": new Audio("./sounds/Flauta/F3.mp3"),
+        "fa": new Audio("./sounds/Flauta/F4.mp3")
     },
     "piano": {
-        do: "piano",
-        re: new Audio(""),
-        mi: new Audio(""),
-        fa: new Audio("")
+        "silencio": new Audio("./sounds/silencio.mp3"),
+        "do": new Audio("./sounds/Piano/P1.mp3"),
+        "re": new Audio("./sounds/Piano/P2.mp3"),
+        "mi": new Audio("./sounds/Piano/P3.mp3"),
+        "fa": new Audio("./sounds/Piano/P4.mp3")
     },
     "xilofono": {
-        do: "xilofono",
-        re: new Audio(""),
-        mi: new Audio(""),
-        fa: new Audio("")
+        "silencio": new Audio("./sounds/silencio.mp3"),
+        "do": new Audio("./sounds/Xilofono/X1.mp3"),
+        "re": new Audio("./sounds/Xilofono/X2.mp3"),
+        "mi": new Audio("./sounds/Xilofono/X3.mp3"),
+        "fa": new Audio("./sounds/Xilofono/X4.mp3")
     }
 }
 
